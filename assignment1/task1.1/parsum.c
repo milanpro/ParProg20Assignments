9d18e870628f61ab1a3e556bf8c49204dc5f4d12
Kolya Opahle <kolyaopahle@gmail.com>
2020-05-15 18:27:14 +0200

    switched longs to uint64_t and used GMP for aggregation (uint64 overflow)

--- assignment1/task1.1/parsum.c
+++ assignment1/task1.1/parsum.c
@@ -1,52 +1,60 @@
+#include "gmp.h"
 #include "stdio.h"
 #include "stdlib.h"
+#include "stdint.h"
 #include "pthread.h"
 #include "math.h"
 
-void *calculate_range(void *range){
-  long start = *((long *)range);
-  long stop = *((long *)range + 1);
+void *calculate_range(void *range)
+{
+  uint64_t start = *((uint64_t *)range);
+  uint64_t stop = *((uint64_t *)range + 1);
 
-  long sum = 0;
+  mpz_t *sum = malloc(sizeof(mpz_t));
+  mpz_init(*sum);
 
-  for (long i = start; i <= stop; i++){
-      sum += i;
+  for (uint64_t i = start; i <= stop; i++)
+  {
+    mpz_add_ui(*sum, *sum, i);
   }
 
-  return (void*) sum;
+  return (void *)sum;
 }
 
 int main(int argc, char const *argv[])
 {
   int num_threads = atoi(argv[1]);
-  long start = atol(argv[2]);
-  long stop = atol(argv[3]);
+  uint64_t start = atol(argv[2]);
+  uint64_t stop = atol(argv[3]);
 
   pthread_t threads[num_threads];
-  long *ranges = (long*) malloc(sizeof(long) * num_threads * 2);
-  long current = 0;
+  uint64_t *ranges = (uint64_t *)malloc(sizeof(uint64_t) * num_threads * 2);
+  uint64_t current = start;
 
-  for (int i = 0; i < num_threads; i++) {
-      double step = (double) (stop - current) / (double) (num_threads - i);
-      long end = current + step;
-      ranges[i * 2] = current;
-      ranges[i * 2 + 1] = end;
+  for (int i = 0; i < num_threads; i++)
+  {
+    double step = (double)(stop - current) / (double)(num_threads - i);
+    uint64_t end = current + step;
+    ranges[i * 2] = current;
+    ranges[i * 2 + 1] = end;
 
-      current = end + 1;
+    current = end + 1;
 
-      int result_code = pthread_create(&threads[i], NULL, calculate_range, &ranges[i * 2]);
+    pthread_create(&threads[i], NULL, calculate_range, &ranges[i * 2]);
   }
 
-  long sum = 0;
+  mpz_t sum;
+  mpz_init(sum);
 
-  for (int i = 0; i < num_threads; i++) {
-      void *result;
-      int result_code = pthread_join(threads[i], &result);
+  for (int i = 0; i < num_threads; i++)
+  {
+    void *result;
+    pthread_join(threads[i], &result);
 
-      sum += (long) result;
+    mpz_add(sum, sum, result);
   }
 
-  printf("%ld", sum);
+  mpz_out_str(stdout, 10, sum);
   free(ranges);
 
   return 0;
