65edb5bea35efdbdddf2d45a0084df60144c14a5
J-L-O <18723283+J-L-O@users.noreply.github.com>
2020-05-19 17:03:49 +0200

    Add output logic

--- assignment1/task1.3/heatmap.c
+++ assignment1/task1.3/heatmap.c
@@ -3,6 +3,7 @@
 #include "stdlib.h"
 #include "string.h"
 #include "unistd.h"
+#include "math.h"
 
 int width;
 int height;
@@ -19,6 +20,13 @@ struct hotspot
   // exclusive
   int endround;
   struct hotspot* next_hotspot;
+};
+
+struct coord
+{
+  int x;
+  int y;
+  struct coord* next_coord;
 };
 
 struct hotspot* hotspot_list;
@@ -74,6 +82,7 @@ void initialize(const char *hotspots_filename)
     new_hotspot->startround = atoi(token);
     token = strtok(NULL, ",");
     new_hotspot->endround = atoi(token);
+    new_hotspot->next_hotspot = NULL;
 
     last_hotspot = new_hotspot;
   }
@@ -85,7 +94,7 @@ void initialize(const char *hotspots_filename)
 
 double get_value(double *from, int x, int y)
 {
-  if ((x < 0 || x > width) && (y < 0 || y > height)) {
+  if ((x < 0 || x > width) || (y < 0 || y > height)) {
     return 0;
   }
 
@@ -95,6 +104,105 @@ double get_value(double *from, int x, int y)
 void set_value(double *from, int x, int y, double value)
 {
   from[width * y + x] = value;
+}
+
+struct coord *read_coords(const char *coords_filename)
+{
+  struct coord* coord_list;
+
+  FILE *fp;
+  fp = fopen(coords_filename, "r");
+  char *line = NULL;
+  size_t len = 0;
+
+  if (fp == NULL)
+  {
+    exit(EXIT_FAILURE);
+  }
+
+  int return_code = getline(&line, &len, fp);
+  if (return_code == -1) {
+    exit(EXIT_FAILURE);
+  }
+
+  char *token;
+  struct coord* last_coord = NULL;
+  struct coord* new_coord = NULL;
+  while (getline(&line, &len, fp) != -1)
+  {
+    if (last_coord == NULL) {
+      coord_list = malloc(sizeof(struct coord));
+      new_coord = coord_list;
+    } else {
+      new_coord = malloc(sizeof(struct coord));
+      last_coord->next_coord = new_coord;
+    }
+    token = strtok(line, ",");
+    new_coord->x = atoi(token);
+    token = strtok(NULL, ",");
+    new_coord->y = atoi(token);
+    new_coord->next_coord = NULL;
+
+    last_coord = new_coord;
+  }
+
+  fclose(fp);
+  if (line)
+    free(line);
+
+  return coord_list;
+}
+
+void write_results()
+{
+  FILE * fp;
+  fp = fopen ("output.txt", "w");
+
+  for (int y = 0; y < height; y++)
+  {
+    for (int x = 0; x < width; x++)
+    {
+      double value = get_value(dest, x, y);
+
+      if (value > 0.9)
+      {
+        fprintf (fp, "X");
+      }
+      else
+      {
+        value += 0.09;
+        char output[4];
+
+        snprintf(output, 50, "%.2f", value);
+        if (value > 0.1)
+          printf("Value: %.2f Character: %c\n", value, output[2]);
+        fprintf (fp, "%c", output[2]);
+      }
+    }
+
+    fprintf(fp, "\n");
+  }
+
+  fclose (fp);
+}
+
+void write_results_coords(const char *coords_filename)
+{
+  struct coord* coord_list = read_coords(coords_filename);
+
+  FILE * fp;
+  fp = fopen ("output.txt", "w");
+
+  struct coord* last_coord = coord_list;
+  while (last_coord != NULL)
+  {
+    double value = get_value(dest, last_coord->x, last_coord->y);
+
+    fprintf (fp, "%.4f\n", value);
+    last_coord = last_coord->next_coord;
+  }
+
+  fclose (fp);
 }
 
 void *worker(void *args)
@@ -123,7 +231,7 @@ int main(int argc, char const *argv[])
   height = 20;
   int rounds = 32;
   const char *hotspots_filename = "test_hotspots.csv";
-  const char *coords_filename = "test_coords.csv";
+  const char *coords_filename = ""; //"test_coords.csv";
 
   if (argc != 1)
   {
@@ -177,23 +285,17 @@ int main(int argc, char const *argv[])
         pthread_join(threads[width * y + x], NULL);
       }
     }
-    
-    printf("Round: %d/%d done\n", i, rounds);
-
-    for (int x = 0; x < width; x++)
-    {
-      for (int y = 0; y < height; y++)
-      {
-        printf("%.1f,", get_value(dest, x, y));
-      }
-      printf("\n");
-    }
 
     //Swapping src into dest and the other way round
     double *temp = dest;
     dest = src;
     src = temp;
   }
+
+  if(strcmp(coords_filename, "") != 0)
+    write_results_coords(coords_filename);
+  else
+    write_results();
 
   return 0;
 }
