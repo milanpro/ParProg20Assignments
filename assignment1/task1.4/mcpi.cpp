1984fc50bd58155f7df78931598501ed25e1b911
Milan Proell <m.proell@sap.com>
2020-05-20 12:15:06 +0200

    Started task 4

--- assignment1/task1.4/mcpi.cpp
+++ assignment1/task1.4/mcpi.cpp
@@ -1,18 +1,66 @@
 #include <iostream>
 #include <thread>
- 
-void foo() 
-{
-  std::cout << "doing great.\n";
+#include <random>
+#include <future>
+
+enum Strategy {
+  LOCAL = 1,
+  SYNC = 2,
+  ATOMIC = 3
+};
+
+Strategy execution_strategy;
+
+bool in_circle(double x, double y) {
+  return (x*x) + (y*y) <= 1.0f;
 }
 
-int main() 
+uint64_t monte_carlo_worker(uint64_t npoints, std::promise<uint64_t> && result) 
 {
-  std::thread first (foo);
-  std::thread second (foo);
+  std::thread::id thread_id = std::this_thread::get_id();
+  std::mt19937 generator(thread_id);
+  std::uniform_real_distribution<double> dist(0.0, 1.0);
 
-  first.join();
-  second.join();
+  uint64_t circle_points = 0;
+
+  for (int i = 0; i < npoints; i++) {
+    if (in_circle(dist(generator), dist(generator))) {
+      circle_points++;
+    }
+  }
+
+  result.set_value(circle_points);
+}
+
+int main(int argc, char * argv[]) 
+{
+  uint64_t point_count = atoll(argv[1]);
+  int thread_count = atoi(argv[2]);
+  execution_strategy = static_cast<Strategy>(atoi(argv[3]));
+
+  std::thread threads[thread_count];
+  std::promise<uint64_t> in_circle_counts[thread_count];
+
+  uint64_t thread_points = point_count / thread_count;
+  int remainder = point_count % thread_count;
+  for (int i = 0; i < thread_count; i++)
+  {
+    if (i == thread_count - 1 ) {
+      thread_points += remainder;
+    }
+    threads[i] = std::thread(monte_carlo_worker, thread_points, std::move());
+  }
+
+  int in_circle_acc = 0;
+  for (int i = 0; i < thread_count; i++)
+  {
+    threads[i].join();
+    if (execution_strategy == Strategy::LOCAL)
+    {
+      in_circle_acc += in_circle_counts[i].
+    }
+    
+  }
 
   std::cout << "foo\n";
 
