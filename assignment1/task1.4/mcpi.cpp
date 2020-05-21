3a768d4cb9844c999bf8f9ad7a13a99b5976cd62
J-L-O <18723283+J-L-O@users.noreply.github.com>
2020-05-21 16:42:01 +0200

    (1.4) Prepare for more execution strategies

--- assignment1/task1.4/mcpi.cpp
+++ assignment1/task1.4/mcpi.cpp
@@ -12,6 +12,8 @@ enum Strategy
 
 Strategy execution_strategy;
 
+std::atomic<int> global_circle_points;
+
 bool in_circle(double x, double y)
 {
   return (x * x) + (y * y) <= 1.0f;
@@ -36,12 +38,8 @@ void monte_carlo_worker(uint64_t npoints, std::promise<uint64_t> &&result)
   result.set_value(circle_points);
 }
 
-int main(int argc, char *argv[])
+double mcpi_local(uint64_t point_count, int thread_count)
 {
-  uint64_t point_count = atoll(argv[1]);
-  int thread_count = atoi(argv[2]);
-  execution_strategy = static_cast<Strategy>(atoi(argv[3]));
-
   std::thread threads[thread_count];
   std::promise<uint64_t> in_circle_promises[thread_count];
   std::future<uint64_t> in_circle_futures[thread_count];
@@ -73,6 +71,39 @@ int main(int argc, char *argv[])
   }
 
   double pi = 4 * (double)in_circle_acc / (double)point_count;
+
+  return pi;
+}
+
+double mcpi_sync(uint64_t point_count, int thread_count)
+{
+  return 0.0;
+}
+
+double mcpi_atomic(uint64_t point_count, int thread_count)
+{
+  return 0.0;
+}
+
+int main(int argc, char *argv[])
+{
+  uint64_t point_count = atoll(argv[1]);
+  int thread_count = atoi(argv[2]);
+  execution_strategy = static_cast<Strategy>(atoi(argv[3]));
+
+  double pi;
+
+  switch(execution_strategy) {
+    case LOCAL:
+      pi = mcpi_local(point_count, thread_count);
+      break;
+    case SYNC:
+      pi = mcpi_sync(point_count, thread_count);
+      break;
+    default:
+      pi = mcpi_atomic(point_count, thread_count);
+      break;
+  }
 
   std::cout << "PI: " << pi << "\n";
 
