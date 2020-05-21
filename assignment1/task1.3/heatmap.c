30a3874d71b7e089b34f50c947a2208f1784f722
J-L-O <18723283+J-L-O@users.noreply.github.com>
2020-05-21 16:05:53 +0200

    (1.3) Use only as many threads as there are cores

--- assignment1/task1.3/heatmap.c
+++ assignment1/task1.3/heatmap.c
@@ -3,31 +3,56 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/sysinfo.h>
 #include "csv.h"
 
 int width;
 int height;
+int core_count;
 
 double *src;
 double *dest;
 
 struct hotspot *hotspot_list;
 
-struct dest_target
+struct task_range
 {
-  int x;
-  int y;
+  int start_x;
+  int start_y;
+
+  int count;
 };
 
-struct dest_target *targets;
+struct task_range *ranges;
+
+void assign_ranges()
+{
+  int x = 0, y = 0;
+  int current = 0;
+  int total = width * height;
+  
+  for (int i = 0; i < core_count; i++)
+  {
+    ranges[i].start_x = x;
+    ranges[i].start_y = y;
+
+    int step = (int) ((double)(total - current) / (double)(core_count - i));
+    ranges[i].count = step;
+    current += step;
+
+    y += (x + step) / width;
+    x = (x + step) % width;
+  }
+}
 
 void initialize(const char *hotspots_filename)
 {
   src = calloc(width * height, sizeof(double));
   dest = calloc(width * height, sizeof(double));
-  targets = calloc(width * height, sizeof(struct dest_target));
+  ranges = calloc(core_count, sizeof(struct task_range));
 
   hotspot_list = parse_hotspot_list(hotspots_filename);
+  assign_ranges();
 }
 
 double get_value(double *from, int x, int y)
@@ -97,24 +122,57 @@ void write_results_coords(const char *coords_filename)
   fclose(fp);
 }
 
-void *worker(void *args)
+void compute_point(int x_coord, int y_coord)
 {
-  struct dest_target *target = (struct dest_target *)args;
   double acc = 0;
 
   for (int x = -1; x <= 1; x++)
   {
     for (int y = -1; y <= 1; y++)
     {
-      acc += get_value(src, x + target->x, y + target->y);
+      acc += get_value(src, x_coord + x, y_coord + y);
     }
   }
 
   acc /= 9;
 
-  set_value(dest, target->x, target->y, acc);
+  set_value(dest, x_coord, y_coord, acc);
+}
+
+void *worker(void *args)
+{
+  struct task_range *slice = (struct task_range *) args;
+
+  int col = slice->start_x;
+  int row = slice->start_y;
+
+  for (int i = 0; i < slice->count; i++)
+  {
+    compute_point(col, row);
+
+    col++;
+    if (col == width)
+    {
+      col = 0;
+      row++;
+    }
+  }
 
   return NULL;
+}
+
+void set_hotspots(int round)
+{
+  struct hotspot *last_hotspot = hotspot_list;
+  while (last_hotspot != NULL)
+  {
+    if (round >= last_hotspot->startround && round < last_hotspot->endround)
+    {
+      set_value(src, last_hotspot->x, last_hotspot->y, 1.0f);
+    }
+
+    last_hotspot = last_hotspot->next_hotspot;
+  }
 }
 
 int main(int argc, char const *argv[])
@@ -124,6 +182,8 @@ int main(int argc, char const *argv[])
   int rounds = 32;
   const char *hotspots_filename = "test_hotspots.csv";
   const char *coords_filename = NULL; //"test_coords.csv";
+
+  core_count = get_nprocs();
 
   if (argc != 1)
   {
@@ -141,56 +201,24 @@ int main(int argc, char const *argv[])
     }
   }
 
-  printf("Width: %d Height: %d Rounds: %d\n", width, height, rounds);
-
   initialize(hotspots_filename);
 
-  pthread_t threads[width * height];
+  pthread_t threads[core_count];
 
-  struct hotspot *last_hotspot = hotspot_list;
-  while (last_hotspot != NULL)
-  {
-    if (0 >= last_hotspot->startround && 0 < last_hotspot->endround)
-    {
-      set_value(src, last_hotspot->x, last_hotspot->y, 1.0f);
-    }
-
-    last_hotspot = last_hotspot->next_hotspot;
-  }
+  set_hotspots(0);
 
   for (int i = 0; i < rounds; i++)
   {
     printf("Round: %d/%d starting...\n", i, rounds);
 
-    for (int x = 0; x < width; x++)
+    for (int j = 0; j < core_count; j++)
     {
-      for (int y = 0; y < height; y++)
-      {
-        targets[width * y + x].x = x;
-        targets[width * y + x].y = y;
-
-        pthread_create(&threads[width * y + x], NULL,
-                       worker, &targets[width * y + x]);
-      }
+      pthread_create(&threads[j], NULL, worker, &ranges[j]);      
     }
 
-    for (int x = 0; x < width; x++)
+    for (int j = 0; j < core_count; j++)
     {
-      for (int y = 0; y < height; y++)
-      {
-        pthread_join(threads[width * y + x], NULL);
-      }
-    }
-
-    last_hotspot = hotspot_list;
-    while (last_hotspot != NULL)
-    {
-      if (i + 1 >= last_hotspot->startround && i + 1 < last_hotspot->endround)
-      {
-        set_value(dest, last_hotspot->x, last_hotspot->y, 1.0f);
-      }
-
-      last_hotspot = last_hotspot->next_hotspot;
+      pthread_join(threads[j], NULL);
     }
 
     printf("Round: %d/%d done\n", i, rounds);
@@ -199,6 +227,8 @@ int main(int argc, char const *argv[])
     double *temp = dest;
     dest = src;
     src = temp;
+
+    set_hotspots(i + 1);
   }
 
   if (coords_filename != NULL)
