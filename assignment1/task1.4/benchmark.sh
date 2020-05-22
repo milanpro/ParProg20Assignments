65a6e908e51cf90f9ebc72b86e2d238166136248
J-L-O <18723283+J-L-O@users.noreply.github.com>
2020-05-22 17:14:25 +0200

    Formatting

--- assignment1/task1.4/benchmark.sh
+++ assignment1/task1.4/benchmark.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-FILE="measurement.csv"
-if [ -e $FILE ] ; then
-  rm $FILE
+file="measurement.csv"
+if [ -e $file ] ; then
+  rm $file
 fi
 
-touch $FILE
-echo "strategy,nthreads,npoints,runtime" >> $FILE
+touch $file
+echo "strategy,nthreads,npoints,runtime" >> $file
 
-STRATEGIES=(1 2 3)
-NTHREADS=(1 2 3 4 6 8 16 32 64)
-NPOINTS=(10000000 100000000 1000000000)
+strategies=(1 2 3)
+nthreads=(1 2 3 4 6 8 16 32 64)
+npoints=(10000000 100000000 1000000000)
 
-for strat in "${STRATEGIES[@]}" ; do
-  for threadcount in "${NTHREADS[@]}" ; do
-    for pointcount in "${NPOINTS[@]}" ; do
+for strat in "${strategies[@]}" ; do
+  for threadcount in "${nthreads[@]}" ; do
+    for pointcount in "${npoints[@]}" ; do
       for ((i = 0; i < 4; i++)) ; do
         # Redirect stderr to stdout and throw original stdout away
-        RESULT=$(/usr/bin/time --format="%e" ./mcpi "$pointcount" "$threadcount" "$strat" 2>&1 > /dev/null)
-        echo "$strat","$threadcount","$pointcount","$RESULT" >> $FILE
+        result=$(/usr/bin/time --format="%e" ./mcpi "$pointcount" "$threadcount" "$strat" 2>&1 > /dev/null)
+        echo "$strat","$threadcount","$pointcount","$result" >> $file
       done
     done
   done
