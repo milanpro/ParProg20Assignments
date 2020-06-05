279db81c71a3815a68fcf7099d3883c1992891d3
J-L-O <18723283+J-L-O@users.noreply.github.com>
2020-06-05 13:22:10 +0200

    (2.1) Add naive OMP implementation

--- assignment2/task2.1/heatmap.c
+++ assignment2/task2.1/heatmap.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#include <pthread.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/sysinfo.h>
 #include "csv.h"
+#include <omp.h>
 
 int width;
 int height;
@@ -139,28 +139,6 @@ void compute_point(int x_coord, int y_coord)
   set_value(dest, x_coord, y_coord, acc);
 }
 
-void *worker(void *args)
-{
-  struct task_range *slice = (struct task_range *) args;
-
-  int col = slice->start_x;
-  int row = slice->start_y;
-
-  for (int i = 0; i < slice->count; i++)
-  {
-    compute_point(col, row);
-
-    col++;
-    if (col == width)
-    {
-      col = 0;
-      row++;
-    }
-  }
-
-  return NULL;
-}
-
 void set_hotspots(int round)
 {
   struct hotspot *last_hotspot = hotspot_list;
@@ -203,22 +181,22 @@ int main(int argc, char const *argv[])
 
   initialize(hotspots_filename);
 
-  pthread_t threads[core_count];
-
   set_hotspots(0);
 
   for (int i = 0; i < rounds; i++)
   {
     printf("Round: %d/%d starting...\n", i, rounds);
 
-    for (int j = 0; j < core_count; j++)
+    #pragma omp parallel for
+    for (int x = 0; x < width; x++)
     {
-      pthread_create(&threads[j], NULL, worker, &ranges[j]);      
-    }
 
-    for (int j = 0; j < core_count; j++)
-    {
-      pthread_join(threads[j], NULL);
+      printf("Thread number: %d, x: %d\n", omp_get_thread_num(), x);
+
+      for (int y = 0; y < height; y++)
+      {
+        compute_point(x, y);
+      }
     }
 
     printf("Round: %d/%d done\n", i, rounds);
