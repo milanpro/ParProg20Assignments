e1d054fa09f180cb6bde7bdb8ee5531b149eba4d
J-L-O <18723283+J-L-O@users.noreply.github.com>
2020-06-05 17:21:53 +0200

    (2.2) Read input files

--- assignment2/task2.2/decrypt.cpp
+++ assignment2/task2.2/decrypt.cpp
@@ -1,4 +1,36 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+
+std::vector<std::string> usernames;
+std::vector<std::string> salts;
+std::vector<std::string> hashes;
+std::vector<std::string> dictionary;
+
+void read_passwds(std::string filename)
+{
+  std::ifstream passwd_file(filename);
+
+  std::string line;
+  while (std::getline(passwd_file, line))
+  {
+    int sep = line.find(":");
+    usernames.push_back(line.substr(0, sep));
+    salts.push_back(line.substr(sep + 1, sep + 3));
+    salts.push_back(line.substr(sep + 3));
+  }
+}
+
+void read_dictionary(std::string filename)
+{
+  std::ifstream dict_file(filename);
+
+  std::string line;
+  while (std::getline(dict_file, line))
+  {
+    dictionary.push_back(line);
+  }
+}
 
 int main(int argc, char *argv[])
 {
@@ -11,7 +43,8 @@ int main(int argc, char *argv[])
     dict_filename = argv[2];
   }
 
-  std::cout << passwd_filename << "\n" << dict_filename << "\n";
-
+  read_passwds(passwd_filename);
+  read_dictionary(dict_filename);
+  
   return 0;
 }
