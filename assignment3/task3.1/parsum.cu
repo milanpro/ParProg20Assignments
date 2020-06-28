99713923cf48fce1b052860d77ded5710ab1f97f
Kolya Opahle <kolyaopahle@gmail.com>
2020-06-28 22:43:23 +0200

    please

--- assignment3/task3.1/parsum.cu
+++ assignment3/task3.1/parsum.cu
@@ -3,17 +3,25 @@
 
 #include <iostream>
 #include <numeric>
+#include <math.h>
 
-__global__ void sum(int *output, int chunkSize, int numThreads, int start, int stop)
+__global__ void sum(long *output, long chunkSize, int numThreads, long start, long stop)
 {
     int index = (threadIdx.x + blockIdx.x * blockDim.x);
-    int result = 0;
-    int startNum = start + (index * chunkSize);
-    for (int i = startNum; i < stop && i < (startNum + chunkSize); i++) {
+    long result = 0;
+    long startNum = start + (index * chunkSize);
+
+    for (long i = startNum; i <= stop && i < (startNum + chunkSize); i++) {
         result += i;
     }
 
-    output[index] = result
+    if (result != 0) {
+        //printf("startNum: %ld + chunkSize: %ld = result: %ld\n", startNum, startNum + chunkSize, result);
+    }
+
+    output[index] = result;
+
+    __syncthreads();
 
     auto step_size = 1;
     int number_of_threads = numThreads;
@@ -23,18 +31,19 @@ __global__ void sum(int *output, int chunkSize, int numThreads, int start, int s
         {
             const auto first = index * step_size * 2;
             const auto second = first + step_size;
-            input[first] += input[second];
+            output[first] += output[second];
         }
 
         step_size <<= 1;
         number_of_threads >>= 1;
+        __syncthreads();
     }
 }
 
 int main(int argc, char const *argv[])
 {
     long start = 1;
-    long stop = 6074001000;
+    long stop = 60000;
   
     if (argc == 3)
     {
@@ -45,22 +54,26 @@ int main(int argc, char const *argv[])
     int blockSize;      // The launch configurator returned block size 
     int minGridSize;    // The minimum grid size needed to achieve the maximum occupancy for a full device launch 
 
-    cudaOccupancyMaxPotentialBlockSize(&minGridSize, &blockSize, sum, 0, 0); 
+    cudaOccupancyMaxPotentialBlockSize(&minGridSize, &blockSize, sum, 0, 0);
 
     int numThreads = minGridSize * blockSize;
-    int chunkSize = (stop - start) / numThreads;
+    long chunkSize = ceil((stop - start) / (double)numThreads);
 
-    const int size = numThreads * sizeof(int);
+    if (chunkSize < 1) {
+        chunkSize = 1;
+    }
 
-    int *d;
+    const long size = numThreads * sizeof(long);
+
+    long *d;
 
     cudaMalloc(&d, size);
     cudaMemset(d, 0, size);
 
-    sum<<<1, count / 2>>>(d, chunkSize, numThreads, start, stop);
+    sum<<<minGridSize, blockSize>>>(d, chunkSize, numThreads, start, stop);
 
-    int result;
-    cudaMemcpy(&result, d, sizeof(int), cudaMemcpyDeviceToHost);
+    long result;
+    cudaMemcpy(&result, d, sizeof(long), cudaMemcpyDeviceToHost);
 
     std::cout << "Sum is " << result << std::endl;
 
