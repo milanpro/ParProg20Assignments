24cf8d0c83e72ea9bb977dab0029a976eb616740
Kolya Opahle <kolyaopahle@gmail.com>
2020-06-29 17:29:08 +0200

    switched parsum implementations to working by default

--- assignment3/task3.1/parsum2.cu
+++ assignment3/task3.1/parsum2.cu
@@ -3,119 +3,81 @@
 
 #include <iostream>
 #include <numeric>
-#include <cmath>
+#include <math.h>
 
-typedef unsigned long long int number_type;
-
-__global__ void sum(number_type *input)
+__global__ void sum(long *output, long chunkSize, int numThreads, long start, long stop)
 {
-    const int tid = blockIdx.x * 256 + threadIdx.x;
+    int index = (threadIdx.x + blockIdx.x * blockDim.x);
+    long result = 0;
+    long startNum = start + (index * chunkSize);
 
-    number_type start = input[tid * 2];
-    number_type stop = input[tid * 2 + 1];
+    for (long i = startNum; i <= stop && i < (startNum + chunkSize); i++) {
+        result += i;
+    }
 
-    // Reuse the input array to save the result
-    input[tid * 2] = 0;
-    input[tid * 2 + 1] = 0;
+    if (result != 0) {
+        //printf("startNum: %ld + chunkSize: %ld = result: %ld\n", startNum, startNum + chunkSize, result);
+    }
 
-    for (number_type i = start; i <= stop; i++)
+    output[index] = result;
+
+    __syncthreads();
+
+    auto step_size = 1;
+    int number_of_threads = numThreads;
+    while (number_of_threads > 0)
     {
-        number_type old = input[tid * 2 + 1];
-        input[tid * 2 + 1] += i;
-
-        // Detect overflow
-        if (old > input[tid * 2 + 1])
+        if (index < number_of_threads) // still alive?
         {
-            input[tid * 2]++;
+            const auto first = index * step_size * 2;
+            const auto second = first + step_size;
+            output[first] += output[second];
         }
+
+        step_size <<= 1;
+        number_of_threads >>= 1;
+        __syncthreads();
     }
 }
 
 int main(int argc, char const *argv[])
 {
-    number_type start = 1;
-    number_type stop = 1000;
-
+    long start = 1;
+    long stop = 60000;
+  
     if (argc == 3)
     {
-        start = atol(argv[1]);
-        stop = atol(argv[2]);
+      start = atol(argv[1]);
+      stop = atol(argv[2]);
     }
 
+    int blockSize;      // The launch configurator returned block size 
+    int minGridSize;    // The minimum grid size needed to achieve the maximum occupancy for a full device launch 
 
-    const number_type range_per_thread = min(stop - start, 100ull);
-    const number_type threads_per_block = 256;
+    cudaOccupancyMaxPotentialBlockSize(&minGridSize, &blockSize, sum, 0, 0);
 
-    number_type num_threads = (stop - start + 1) / range_per_thread;
-    number_type num_blocks = std::ceil((double) num_threads / (double) threads_per_block);
+    int numThreads = minGridSize * blockSize;
+    long chunkSize = ceil((stop - start) / (double)numThreads);
 
-    number_type size = sizeof(number_type) * num_blocks * threads_per_block * 2;
-    number_type *ranges = (number_type *)malloc(size);
-    number_type current = start;
-
-
-    for (int i = 0; i < num_threads; i++)
-    {
-        unsigned long long range_per_thread = (double)(stop - current) / (double)(num_threads - i);
-
-        unsigned long long end = current + range_per_thread;
-        ranges[i * 2] = current;
-        ranges[i * 2 + 1] = end;
-
-        current = end + 1;
+    if (chunkSize < 1) {
+        chunkSize = 1;
     }
 
-    number_type *ranges_gpu;
-    cudaMalloc(&ranges_gpu, size);
-    cudaMemcpy(ranges_gpu, ranges, size, cudaMemcpyHostToDevice);
+    const long size = numThreads * sizeof(long);
 
-    sum<<<num_blocks, threads_per_block>>>(ranges_gpu);
+    long *d;
 
-    /*
-    cudaError_t err = cudaGetLastError();
-    const char* error = cudaGetErrorString(err);
-    printf("%s", error)
-     */
+    cudaMalloc(&d, size);
+    cudaMemset(d, 0, size);
 
-    cudaMemcpy(ranges, ranges_gpu, size, cudaMemcpyDeviceToHost);
+    sum<<<minGridSize, blockSize>>>(d, chunkSize, numThreads, start, stop);
 
+    long result;
+    cudaMemcpy(&result, d, sizeof(long), cudaMemcpyDeviceToHost);
 
-    __uint128_t result = 0;
+    std::cout << "Sum is " << result << std::endl;
 
-    for (number_type i = 0; i < num_threads; i++)
-    {
-        __uint128_t upper = ranges[i * 2];
-        __uint128_t lower = ranges[i * 2 + 1];
-
-        upper <<= 64;
-
-        result += upper;
-        result += lower;
-    }
-
-    char arr[39];
-    char basechars[] = "0123456789";
-    int length = 0;
-    while (result != 0)
-    {
-        int radix;
-        radix = result % 10;
-        result = result / 10;
-        arr[length++] = basechars[radix];
-    }
-    if (length == 0)
-    {
-        std::cout << 0 << std::endl;
-    }
-    else
-    {
-        while (length--)
-            std::cout << arr[length];
-
-        std::cout << std::endl;
-    }
-
-    cudaFree(ranges_gpu);
+    cudaFree(d);
 
     return 0;
 }
